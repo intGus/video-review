c762f2e806adee9e1c98899e780bea396075145e
intGus <46197948+intGus@users.noreply.github.com>
2022-11-08 20:53:30 -0500

    Refactor the javascript as functions

--- script.js
+++ script.js
@@ -20,7 +20,36 @@ function onPlayerReady(event) {
 function onPlayerStateChange(event) {
 ;
 }
-logList = document.getElementById("log")
+
+function addComment(user, timestamp, timecode, comment) {
+  const logList = document.getElementById("log")
+  const li = document.createElement("li")
+  li.dataset.timecode = timecode;
+  li.classList.add('comment-item');
+  const commentHeader = document.createElement("div")
+  commentHeader.classList.add('comment-header')
+  const userImg = document.createElement("img")
+  userImg.classList.add('avatar', 'mr-2')
+  userImg.src = "https://ui-avatars.com/api/?background=0D8ABC&color=fff&rounded=true&name=" + encodeURIComponent(user)
+  const userSpan = document.createElement('span')
+  userSpan.textContent = user;
+  userSpan.classList.add('mr-2');
+  const dateSpan = document.createElement('span')
+  dateSpan.textContent = timestamp; //datetime now
+  commentHeader.appendChild(userImg);
+  commentHeader.appendChild(userSpan);
+  commentHeader.appendChild(dateSpan);
+  li.appendChild(commentHeader);
+
+  const pComment = document.createElement('p');
+  pComment.innerHTML = `<span>${timecode}</span> ${comment.value}`
+  logList.appendChild(li)
+  li.appendChild(pComment);
+  
+  
+
+}
+
 text = document.getElementById("comment")
 text.addEventListener('click', function() {
   player.pauseVideo()
@@ -33,32 +62,37 @@ button = document.getElementById("save")
 button.addEventListener('click', function() {
   let timecode = player.getCurrentTime()
   timecode = (timecode.toFixed(2) + "").padStart(5, "0");
-  
-  const li = document.createElement("li")
-  li.dataset.timecode = timecode;
-  li.classList.add('comment-item');
-  
-  const commentHeader = document.createElement("div")
-  commentHeader.classList.add('comment-header')
-  const userImg = document.createElement("img")
-  userImg.classList.add('avatar', 'mr-2')
-  userImg.src = "https://ui-avatars.com/api/?background=0D8ABC&color=fff&rounded=true"
-  const userSpan = document.createElement('span')
-  userSpan.textContent = 'John Doe';
-  userSpan.classList.add('mr-2');
-  const dateSpan = document.createElement('span')
-  dateSpan.textContent = '3d'; //datetime now
-  commentHeader.appendChild(userImg);
-  commentHeader.appendChild(userSpan);
-  commentHeader.appendChild(dateSpan);
-  li.appendChild(commentHeader);
+  let timestamp = '3d'
+  let 
 
-  const pComment = document.createElement('p');
-  pComment.innerHTML = `<span>${timecode}</span> ${comment.value}`
-  logList.appendChild(li)
-  li.appendChild(pComment);
-  comment.value = ''
   document.getElementById("timecode").textContent = '--:--'
+  comment.value = ''
+  
+  // const li = document.createElement("li")
+  // li.dataset.timecode = timecode;
+  // li.classList.add('comment-item');
+  
+  // const commentHeader = document.createElement("div")
+  // commentHeader.classList.add('comment-header')
+  // const userImg = document.createElement("img")
+  // userImg.classList.add('avatar', 'mr-2')
+  // userImg.src = "https://ui-avatars.com/api/?background=0D8ABC&color=fff&rounded=true"
+  // const userSpan = document.createElement('span')
+  // userSpan.textContent = 'John Doe';
+  // userSpan.classList.add('mr-2');
+  // const dateSpan = document.createElement('span')
+  // dateSpan.textContent = '3d'; //datetime now
+  // commentHeader.appendChild(userImg);
+  // commentHeader.appendChild(userSpan);
+  // commentHeader.appendChild(dateSpan);
+  // li.appendChild(commentHeader);
+
+  // const pComment = document.createElement('p');
+  // pComment.innerHTML = `<span>${timecode}</span> ${comment.value}`
+  // logList.appendChild(li)
+  // li.appendChild(pComment);
+  // comment.value = ''
+  // document.getElementById("timecode").textContent = '--:--'
 })
 logList.addEventListener('click', function(e){
   let timecode = e.target.closest("[data-timecode]").dataset.timecode;

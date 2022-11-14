cb9570717fbd3aaf0f1b03b6c682182db2c64ea4
intGus <46197948+intGus@users.noreply.github.com>
2022-11-14 09:23:53 -0500

    Add backend support

--- script.js
+++ script.js
@@ -2,6 +2,20 @@
 var tag = document.createElement('script');
 tag.id = 'iframe-video';
 tag.src = 'https://www.youtube.com/iframe_api';
+
+fetch("http://192.168.1.100:3000/backend/comments")
+.then(res => res.json())
+.then(data => {
+  data.forEach(item => {
+    addComment(
+      item.user,
+      item.timestamp,
+      item.timecode, 
+      item.comment
+    )
+  })
+})
+
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
@@ -24,7 +38,6 @@ function onPlayerStateChange(event) {
 const logList = document.getElementById("log")
 
 function addComment(user, timestamp, timecode, comment) {
-  
   const li = document.createElement("li")
   li.dataset.timecode = timecode;
   li.classList.add('comment-item');
@@ -37,12 +50,11 @@ function addComment(user, timestamp, timecode, comment) {
   userSpan.textContent = user;
   userSpan.classList.add('mr-2');
   const dateSpan = document.createElement('span')
-  dateSpan.textContent = timestamp; //datetime now
+  dateSpan.textContent = timestamp;
   commentHeader.appendChild(userImg);
   commentHeader.appendChild(userSpan);
   commentHeader.appendChild(dateSpan);
   li.appendChild(commentHeader);
-
   const pComment = document.createElement('p');
   pComment.innerHTML = `<span>${timecode}</span> ${comment}`
   logList.appendChild(li)
@@ -53,47 +65,31 @@ comment = document.getElementById("comment")
 comment.addEventListener('click', function() {
   player.pauseVideo()
   let timecode = player.getCurrentTime()
-  timecode = (timecode.toFixed(2) + "").padStart(5, "0");
+  timecode = (timecode.toFixed(2) + "").padStart(5, "0"); //create function
   document.getElementById("timecode").textContent = timecode;
   })
 
 button = document.getElementById("save")
 button.addEventListener('click', function() {
   let timecode = player.getCurrentTime();
-  timecode = (timecode.toFixed(2) + "").padStart(5, "0");
+  timecode = (timecode.toFixed(2) + "").padStart(5, "0"); //create function
   let timestamp = '2d';
-  user = 'Gustavo Diaz';
+  user = 'Andres David';
+  const commentDB = {user, timestamp, timecode, comment: comment.value}
+  fetch("http://192.168.1.100:3000/backend/comments", {
+    method: "POST",
+    headers: {
+      "Content-Type": 'application/json',
+    },
+    body: JSON.stringify(commentDB)
+  })
+  .then(res => console.log(res.json()))
   addComment(user, timestamp, timecode, comment.value);
 
   document.getElementById("timecode").textContent = '--:--'
   comment.value = ''
-  
-  // const li = document.createElement("li")
-  // li.dataset.timecode = timecode;
-  // li.classList.add('comment-item');
-  
-  // const commentHeader = document.createElement("div")
-  // commentHeader.classList.add('comment-header')
-  // const userImg = document.createElement("img")
-  // userImg.classList.add('avatar', 'mr-2')
-  // userImg.src = "https://ui-avatars.com/api/?background=0D8ABC&color=fff&rounded=true"
-  // const userSpan = document.createElement('span')
-  // userSpan.textContent = 'John Doe';
-  // userSpan.classList.add('mr-2');
-  // const dateSpan = document.createElement('span')
-  // dateSpan.textContent = '3d'; //datetime now
-  // commentHeader.appendChild(userImg);
-  // commentHeader.appendChild(userSpan);
-  // commentHeader.appendChild(dateSpan);
-  // li.appendChild(commentHeader);
-
-  // const pComment = document.createElement('p');
-  // pComment.innerHTML = `<span>${timecode}</span> ${comment.value}`
-  // logList.appendChild(li)
-  // li.appendChild(pComment);
-  // comment.value = ''
-  // document.getElementById("timecode").textContent = '--:--'
 })
+
 logList.addEventListener('click', function(e){
   let timecode = e.target.closest("[data-timecode]").dataset.timecode;
   player.seekTo(timecode);
